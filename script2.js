3d2040bf5a5c3d967b16b6c7b5e1f1f44563622c
Muthu-Ajay <muthuajay2001@gmail.com>
2024-06-23 23:22:48 -0800

    form modification

--- script2.js
+++ script2.js
@@ -1,70 +1,25 @@
-const form = document.querySelector("#form");
-const username = document.querySelector("#username");
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-const cpassword = document.querySelector("#cpassword");
-const button = document.querySelector("#submit")
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    validateInputs();
-});
-
-function validateInputs(){
-
-    const usernameVal = username.value.trim();
-    const emailVal = email.value.trim();
-    const passwordVal = password.value.trim();
-    const cpasswordVal = cpassword.value.trim();
-
-    if(usernameVal===''){
-        setError(username,'Username is required');
-    }
-    else{
-        setSuccess(username);
-    }
-    if(emailVal===''){
-        setError(email,'Email is required');
-    }
-    else{
-        setSuccess(email);
-    }
-    if(passwordVal===''){
-        setError(password,'Password is required');
-    }
-    else if(passwordVal.length<8){
-        setError(password,'Password must have atleast 8 characters');
-    }
-    else{
-        setSuccess(password);
-    }
-    if(cpasswordVal===''){
-        setError(cpassword,'Confirm password is required');
-    }
-    else if(cpasswordVal!==passwordVal){
-        setError(cpassword,'Password does not match');
-    }
-    else{
-        setSuccess(cpassword);
-    }
-}
-
-function setError(element,message){
-
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error');
-
-    errorElement.innerText = message;
-    inputGroup.classList.add('error');
-    inputGroup.classList.remove('success');
-}
-
-function setSuccess(element){
-    
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error');
-
-    errorElement.innerText = ''
-    inputGroup.classList.add('success');
-    inputGroup.classList.remove('error');
-}
+$(document).ready(function(){
+    $("#form").validate({
+        errorClass: 'error',
+        rules: {
+            user: {
+                required: true,
+                minlength: 10,
+            
+            },
+            email: {
+                required: true,
+            },
+            pass: {
+                required: true,
+                minlength: 8
+            },
+            cpass: {
+                required:  true,
+                minlength: 8,
+                equalTo: "pass"
+            }
+        }
+      
+    })
+})
